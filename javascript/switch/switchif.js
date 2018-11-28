43f4b70cdfeabf20739707ce01446dee2d6d9fa2
zadhkiel <jose1wilson1tj@gmail.com>
2018-11-27 23:25:33 -0400

    Create switchif.js
    asd

---
+++ javascript/switch/switchif.js
@@ -0,0 +1,33 @@
+var signo=prompt('Ingrese su signo')
+
+if (signo==='acuario') 
+{
+console.log("El signo Acuario simboliza revolución y el símbolo de este signo representa el éter (símbolo de pureza, en la mitología griega el éter es un elemento mucho más puro y brillante que el aire), sin embargo este signo tiene una simbología bastante compleja; entre lo que podríamos destacar la amistad por encima de todo y el altruismo. Son personas muy solidarias y se identifican rápidamente con cualquier sindicato o cualquier grupo donde se esté llevando a cabo un esfuerzo colectivo.")
+}
+if (signo==='piscis') 
+{
+console.log("Piscis corresponde al duodécimo (12) de los signos del zodíaco")
+}
+if (signo==='aries') 
+{
+console.log("Aries es el primer signo del Zodíaco, de naturaleza positiva y pertenece al elemento fuego. ")
+}
+if (signo==='tauro') 
+{
+console.log("Las personas nacidas entre los días 20 de Abril y 20 de Mayo pertenecen a grupo astrológico Tauro. ")
+}
+
+if (signo=='geminis')
+ {
+console.log("Las personas nacidas entre los días 21 de mayo y 20 de junio se encuentran bajo la influencia del signo zodiacal Géminis. ")
+ }
+
+ if (signo==='cancer') 
+ {
+console.log("Las personas nacidas entre el 21 de junio y el 22 de julio pertenecen al grupo zodiacal Cáncer")
+ }
+ if (signo==='leo') 
+ {
+console.log("quellas personas que han nacido entre el día 23 de julio hasta el 22 de agosto su grupo astrológico es Leo")
+ }
+ 
